8a63379721ba061592966b103e4656d1510937d9
Ghulam Mustafa <gmustufa1255@gmail.com>
2025-09-12 03:06:14 +0500

    Tic Tac Toe

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let newGame = document.getElementById('new-game');
 let resetGame = document.getElementById('reset-game');
 let flag = true;
 
+// Winning combinations
 let Arry = [
    [0, 1, 2],
    [0, 4, 8],
@@ -18,16 +19,18 @@ let Arry = [
    [6, 7, 8],
 ];
 
+// Sound effects
 let sound1 = new Audio("sound/sound1.wav");
 let sound2 = new Audio("sound/sound2.wav");
 let sound3 = new Audio("sound/sound3.wav");
 
+// Function to play sound
 function playSound(audio) {
   audio.currentTime = 0; 
   audio.play();
 }
 
-
+// Event listener for new game button
 newGame.addEventListener("click", () => {
    playSound(sound1);
    showMessage1.style.display = "none"
@@ -37,26 +40,52 @@ newGame.addEventListener("click", () => {
       tic.disabled = false
       tic.innerText=""
    }
+   flag = true
 })
 
+function computerMove(){
+   let emptyBoxes = [];
+   btn.forEach((box, index) => {
+      if (box.innerText === "") {
+         emptyBoxes.push(index);
+      }
+   })
+   // Make a random move for the computer
+   if (emptyBoxes.length > 0) {
+      let randomIndex = emptyBoxes[Math.floor(Math.random() * emptyBoxes.length)];
+      let box = btn[randomIndex];
+      playSound(sound1);
+      box.classList.add("box1");
+      box.innerText = 'O';
+      box.disabled = true;
+      flag = true;
+
+   }
+}
+
+// Event listeners for each button
 btn.forEach((box) => {
    box.addEventListener("click", () => {
-      playSound(sound1);
-      console.log('Click')
       if (flag) {
-         box.classList.add("box1")
-         box.innerText = 'O'
-         flag = false
-      }
-      else {
-         box.innerText = 'X'
-         flag = true
-      }
-      box.disabled = true
-      winnerGame()
-   })
-})
+         playSound(sound1);
+         box.innerText = 'X';
+         box.disabled = true;
+         flag = false;
 
+         if (!winnerGame()){
+            draw();
+            setTimeout(() => {
+               computerMove();
+               winnerGame();
+               draw();
+            }, 500);
+         }
+      }
+   });
+});
+
+
+// Function to check for a winner
 function winnerGame() {
    for (const winner of Arry) {
       let winner1 = btn[winner[0]].innerText;
@@ -69,17 +98,39 @@ function winnerGame() {
             mein.style.display = "none"
             playSound(sound2);
             showMessage(winner3)
+
+            btn.forEach((box) => box.disabled = true);
+
+            return true
          }
       }
    }
 };
 
-function showMessage(game){
+// Function to check for a draw
+function draw() {
+   let allField = [...btn].every((box) => box.innerText !== "");
+   if(allField){
+      mein.style.display="none"
+      playSound(sound3);
+      drawMessage("Draw 🤝")
+   }
+}
 
+// Function to display the draw message
+function drawMessage(draw){
+   showMessage1.style.display = "flex"                                                                                                                                                                                                                              
+   massages1.innerHTML=`<span> ${draw}</span>`
+}
+
+
+// Function to display the winner message
+function showMessage(game){
    showMessage1.style.display = "flex"                                                                                                                                                                                                                              
    massages1.innerHTML=`<span> 🎉 Congratulation</span>,🎉 Winner is ${game}`
 }
 
+// Event listener for reset game button
 resetGame.addEventListener('click', () => {
    for (const resetgame of btn) {
       if(resetgame.innerHTML!= ""){
@@ -89,4 +140,5 @@ resetGame.addEventListener('click', () => {
       resetgame.disabled =  false
       resetgame.innerText = ""
    }
+   flag = true;
 })
